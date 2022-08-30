0a3a9876297f93b3c164f13568a02c71a21205a1
sehee <seeeh0128@gmail.com>
2022-08-30 13:05:08 +0900

    Design: 리뷰 페이지 마크업

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-// import Home from "./pages/Home";
 import About from "./pages/About";
+import Review from "./pages/Review";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<About />} />
+        <Route path="/review" element={<Review />} />
       </Routes>
     </BrowserRouter>
   );
