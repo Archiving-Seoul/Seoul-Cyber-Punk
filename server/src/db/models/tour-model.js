bb88306e1144cf2e1cbfd931d3aa0f01b152b43e
jiyoonZ <juneky416@gmail.com>
2022-09-04 23:00:01 +0900

    Feat : 카테고리별 기능조회 구현

--- server/src/db/models/tour-model.js
+++ server/src/db/models/tour-model.js
@@ -1,11 +1,15 @@
 import {model} from "mongoose";
 import {TourSchema} from "../schemas/tour-schema";
-
 const Tour = model("tourAttraction", TourSchema);
 
 export class TourModel {
+  // 관리자 페이지 spot 추가
   async add(tourInfo) {
     return await Tour.create(tourInfo);
+  }
+  // 카테고리별 spot 조회하기
+  async findByCategory(categoryId) {
+    return await Tour.find({category: categoryId});
   }
 }
 
