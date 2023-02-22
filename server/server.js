aba82dc8f41447a6d7c9e7484d3225b1bdbc9ea1
jiyoonZ <wldbsl416@gmail.com>
2023-02-22 13:06:13 +0900

    Deploy by docker

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
 import "dotenv/config";
 import {app} from "./src/app";
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3306;
 
-app.listen(PORT, () => {
-  console.log(`정상적으로 서버를 시작하였습니다.  http://localhost:${PORT}`);
+app.listen({host: '0.0.0.0', port: PORT}, () => {
+  console.log(`정상적으로 서버를 시작하였습니다.  http://0.0.0.0:${PORT}`);
 });
